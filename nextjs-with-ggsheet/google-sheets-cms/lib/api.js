c952ab4d92cf90fd7ce02ed08d595dd0af27d3af
KevinHuynh666 <53897382+KevinHuynh666@users.noreply.github.com>
2021-01-28 09:31:52 +1000

    Added the code for login page

---
+++ nextjs-with-ggsheet/google-sheets-cms/lib/api.js
@@ -0,0 +1,40 @@
+
+import { google } from "googleapis";
+import marked from "marked";
+const renderer = new marked.Renderer();
+renderer.link = (href, title, text) =>
+    `<a target="_blank" rel="noopener noreferrer" href="${href}" title="${title || ""
+    }">${text}</a>`;
+
+export async function getStudentID() {
+    try {
+        const scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+        const jwt = new google.auth.JWT(
+            process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+            null,
+            // we need to replace the escaped newline characters
+            // https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse
+            process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
+            scopes
+        );
+
+        const sheets = google.sheets({ version: "v4", auth: jwt });
+        const response = await sheets.spreadsheets.values.get({
+            spreadsheetId: process.env.SPREADSHEET_ID,
+            range: "Sheet1",
+        });
+
+        const rows = response.data.values;
+        if (rows.length) {
+            return rows.map((row) => ({
+                studentID: row[0],
+                studentName: row[1]
+            }));
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+
+    return [];
+}
