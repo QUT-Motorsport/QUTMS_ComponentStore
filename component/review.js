7917a3aa20ab4d7502935d2bdf437d4ed6b3542a
KevinHuynh666 <53897382+KevinHuynh666@users.noreply.github.com>
2021-02-12 13:55:56 +1000

    Revert "Add unique ID in review"
    This reverts commit 8b3546f10415278a77995ac941f0a06d98aca4d8.

--- component/review.js
+++ component/review.js
@@ -3,7 +3,6 @@ import Cookies from 'universal-cookie';
 import dynamic from 'next/dynamic'
 import Swal from 'sweetalert2';
 import { update } from '../lib/script'
-const { v1: uuidv1 } = require('uuid');
 
 const Typography = dynamic(() => import('@material-ui/core/Typography'), { ssr: false })
 const Container = dynamic(() => import('@material-ui/core/Container'), { ssr: false })
@@ -43,7 +42,7 @@ function Review(props) {
         // Create an order to send to database
         const order = {
             student_id: props.finalID,
-            student_name: props.finalName,
+            student_name: props.finalName, 
             order_details: cookies.get('order_details')
         }
 
@@ -174,7 +173,7 @@ function Review(props) {
                             <div className="col col-4">Return&nbsp;</div>
                         </li>
                         {carts.map((cart) => (
-                            <li className="table-row" key={uuidv1()}>
+                            <li className="table-row" key={cart.component_name}>
                                 <div className="col col-2">
                                     <img alt="Google" height="50" id="hplogo" src="/img/logo_orange.png" />
                                 </div>
